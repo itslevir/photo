b105513e75e4cb7209c6b08fa7b26c887263f430
Levi <levir620@skiff.com>
2024-10-22 15:53:19 +0000

    font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '../app.css';
+	import '@fontsource/plus-jakarta-sans/latin.css';
 </script>
 
 <div class="h-screen w-screen bg-black p-5 text-white"><slot></slot></div>
 
-<style></style>
+<style>
+	div {
+		font-family: 'Plus Jakarta Sans', sans-serif;
+	}
+</style>
